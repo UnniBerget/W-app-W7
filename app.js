a8928bd960bf297d1ded130a0d4bbc9e610f765c
UnniBerget <96954530+UnniBerget@users.noreply.github.com>
2022-01-10 19:15:49 +0100

    Corr

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "50be4728976a1683c41d638c74b91b409";
+  let apiKey = "0be4728976a1683c41d638c74b91b409";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
